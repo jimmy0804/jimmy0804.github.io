1999b280a5c57a67042f23654eebec2dabacc4b4
Jimmy Yeung <jimmyexe@hotmail.com>
2016-06-01 18:35:00 -0500

    Testing geolocation

--- IPickForYou/javascript/custom.js
+++ IPickForYou/javascript/custom.js
@@ -1,14 +1,27 @@
 $(document).ready(function() {
-  $('#ex1').slider({
-	formatter: function(value) {
-		return 'Current value: ' + value;
-	}
-});
+    $('#ex1').slider({
+  	formatter: function(value) {
+  		return 'Current value: ' + value;
+  	}
+  });
 
-// // Without JQuery
-// var slider = new Slider('#ex1', {
-// 	formatter: function(value) {
-// 		return 'Current value: ' + value;
-// 	}
-// });
+  var x = document.getElementById("demo");
+
+  $('.getLocation').click(function() {
+      getLocation();
+  });
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function showPosition(position) {
+    x.innerHTML = "Latitude: " + position.coords.latitude +
+    "<br>Longitude: " + position.coords.longitude;
+}
+
 });
