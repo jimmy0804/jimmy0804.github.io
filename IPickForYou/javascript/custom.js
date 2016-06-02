fcdc1466854e25bfdb79c796c709eadbec8db4ed
Jimmy Yeung <jimmyexe@hotmail.com>
2016-06-01 19:17:55 -0500

    Formatted custom js and added slider text

--- IPickForYou/javascript/custom.js
+++ IPickForYou/javascript/custom.js
@@ -1,31 +1,43 @@
+// Make sure the website is loaded as https so that chrome won't complaint about geolocation
 var host = "jimmy0804.github.io";
 if ((host == window.location.host) && (window.location.protocol != "https:"))
     window.location.protocol = "https";
 
-$(document).ready(function() {
-    $('#ex1').slider({
-  	formatter: function(value) {
-  		return 'Current value: ' + value;
-  	}
-  });
 
-  var x = document.getElementById("demo");
+// vars.
+var locationResult = document.getElementById("location-result");
+var sliderResult = document.getElementById("slider-result");
 
+function clickActions() {
   $('.getLocation').click(function() {
       getLocation();
   });
+}
+
+function sliderSetUp() {
+      $('#ex1').slider({
+      formatter: function(value) {
+        sliderResult.innerHTML = value
+      }
+    });
+}
 
 function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition);
     } else {
-        x.innerHTML = "Geolocation is not supported by this browser.";
+        locationResult.innerHTML = "Geolocation is not supported by this browser.";
     }
 }
 
 function showPosition(position) {
-    x.innerHTML = "Latitude: " + position.coords.latitude +
+    locationResult.innerHTML = "Latitude: " + position.coords.latitude +
     "<br>Longitude: " + position.coords.longitude;
 }
 
-});
+function init() {
+  sliderSetUp();
+  clickActions();
+}
+
+$(document).ready(init());
