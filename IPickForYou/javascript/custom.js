45ff7363d5cdacf0ac198323e8c5f2c5c72c5be1
Jimmy Yeung <jimmyexe@hotmail.com>
2016-06-01 19:24:29 -0500

    Added code to handle geoLocation error and timeout

--- IPickForYou/javascript/custom.js
+++ IPickForYou/javascript/custom.js
@@ -7,6 +7,9 @@ if ((host == window.location.host) && (window.location.protocol != "https:"))
 // vars.
 var locationResult = document.getElementById("location-result");
 var sliderResult = document.getElementById("slider-result");
+var geoOptions = {
+  timeout: 10 * 1000
+}
 
 function clickActions() {
   $('.getLocation').click(function() {
@@ -17,14 +20,14 @@ function clickActions() {
 function sliderSetUp() {
       $('#ex1').slider({
       formatter: function(value) {
-        sliderResult.innerHTML = value
+        sliderResult.innerHTML = value + "m"
       }
     });
 }
 
 function getLocation() {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
+        navigator.geolocation.getCurrentPosition(showPosition,geoError,geoOptions);
     } else {
         locationResult.innerHTML = "Geolocation is not supported by this browser.";
     }
@@ -33,6 +36,15 @@ function getLocation() {
 function showPosition(position) {
     locationResult.innerHTML = "Latitude: " + position.coords.latitude +
     "<br>Longitude: " + position.coords.longitude;
+}
+
+function geoError(error) {
+  console.log('Error occurred. Error code: ' + error.code);
+   // error.code can be:
+   //   0: unknown error
+   //   1: permission denied
+   //   2: position unavailable (error response from location provider)
+   //   3: timed out
 }
 
 function init() {
